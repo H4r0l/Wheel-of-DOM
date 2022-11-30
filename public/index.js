3b7e4be87614daab3ef8839b090aefbd647c5326
Lily Johana <lilajoha2907@gmail.com>
2022-11-30 12:17:56 -0500

    orden html sweetalert

---
+++ public/index.js
@@ -0,0 +1,15 @@
+//
+swal({
+        title: "¡Bienvenido!",
+        text: "Ingresa los nombres de las victimas:",
+        content: "input",
+        button: {
+            text: "Agregar",
+            closeModal: false,
+        },
+        button: {
+            text: "Inicio",
+            closeModal: false,
+        },
+    })
+
